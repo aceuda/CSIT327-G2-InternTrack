8cc4602334a7cd6df44a15e7316df7b4e51caa9e
Aceucchi <abellaace23@gmail.com>
2025-10-03 23:45:50 +0800

    feature(interntrack_app): complete app setup with login, register, and logout

--- interntrack_project/interntrack_app/models.py
+++ interntrack_project/interntrack_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Intern(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    hours_completed = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+        
 # Create your models here.
--- interntrack_project/interntrack_project/urls.py
+++ interntrack_project/interntrack_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
 ]
