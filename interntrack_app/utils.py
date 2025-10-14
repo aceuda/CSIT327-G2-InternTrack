67a49a7e25d4e90d59d93a0f50825c48715c8355
pj013 <aaronpj013@gmail.com>
2025-10-14 22:17:45 +0800

    add utils.py

---
+++ interntrack_app/utils.py
@@ -0,0 +1,13 @@
+def normalize_student_data(data):
+    return {
+        "year_level": data.get("year_level", "").strip().title(),  # e.g., "3rd Year"
+        "program": data.get("program", "").strip().upper(),          # BSIT
+        "student_id": data.get("student_id"),                         # 18-0668-202
+    }
+
+def normalize_admin_data(data):
+    return {
+        "department": data.get("department", "").strip().title(),  # e.g., "Human Resources"
+        "position": data.get("position", "").strip().title(),      # e.g., "Team Lead"
+        "employee_id": data.get("employee_id", ""),
+    }
